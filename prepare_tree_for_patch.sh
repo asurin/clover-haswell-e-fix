4137ccc381abcb012a8bf7e3bbbf4b557d0ee0cf
Alastair Surin <alastairjsurin@gmail.com>
2017-10-15 13:48:24 -0400

    4235

--- prepare_tree_for_patch.sh
+++ prepare_tree_for_patch.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 PATCH_LOCATION="/Users/asurin/src/edk2/Clover"
 
-TARGET_REV=$(svn info $CLOVER_LOCATION 2>&1 | awk '/^Revision:/ {print $2}')
+if [ -z "$TARGET_REV" ]; then
+  echo "Please set the rev to reset to as TARGET_REV"
+  exit 1
+fi
 
+echo "Resetting tree to $TARGET_REV"
+cd $PATCH_LOCATION && svn status --no-ignore | grep '^[I?]' | cut -c 9- | while IFS= read -r f; do rm -rf "$f"; done
 rm $PATCH_LOCATION/rEFIt_UEFI/Platform/kernel_patcher.c
 rm $PATCH_LOCATION/rEFIt_UEFI/Platform/Settings.c
 rm $PATCH_LOCATION/rEFIt_UEFI/refit/lib.h
